264ae74e1cace4954952b353d40e2ef921177380
ashutosh-vd <ashutoshsensei09@gmail.com>
2025-03-29 19:09:19 +0530

    part1 complete

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cookieparser = require('cookie-parser');
 app.use(cookieparser());
+// console.log('Cookies: ', req.cookies)
 
 app.set('view engine', 'ejs');
 
@@ -21,14 +22,35 @@ app.get('/', (req, res) => {
 	res.render('index');
 })
 
-app.post('/create' , (req, res) => {
+app.post('/create' , async (req, res) => {
 	const {name, username, email, password} = req.body;
+	const existing_user = await usermodel.findOne({email});
+	if(existing_user) {
+		return res.status(500).send("user already exists with specified email");
+	}
 	bcrypt.hash(password, 10, async (err, hash) => {
 		const cuser = await usermodel.create({
 			name, username, email, password: hash,
 		});
-		res.send(cuser);
+		res.redirect('/login');
+		return;
 	})
+})
+
+function isLoggedin(req, res, next) {
+	let _token = req.cookies._token;
+	if(!_token) return res.status(401).send("You must be Logged in");
+	else {
+		const data = jwt.verify(_token, 'shh');
+		// console.log(data);
+		req.user = data;
+	}
+	next();
+}
+
+app.get('/profile', isLoggedin, (req, res) => {
+	// res.send(`Welcome Loggedin User ${req.user.username}`);
+	res.render('profile', {user : req.user});
 })
 
 app.get('/login', (req, res) => {
@@ -43,16 +65,15 @@ app.post('/login', async (req, res) => {
 	}
 	bcrypt.compare(password, user.password, (err, result) => {
 		if(result) {
-			const token = jwt.sign({
-				username: user.username,
-				email: email,
-			}, 'shh', (err, token) =>{
-				res.cookie("_token", token);
-				res.redirect('/login');
+			jwt.sign({username: user.username,email: email,}, 'shh', (err, token) =>{
+					res.cookie("_token", token);
+					res.redirect('/profile');
 			})
+			// const token = jwt.sign(payload, 'shh');
+			// res.cookie('_token', token); 
 		}
 		else {
-			res.send("something_went_wrong");
+			res.status(500).send('something_went_wrong');
 			return;
 		}
 	})
